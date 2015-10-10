73477ec35d75eceb7a08322dc3cf41de1915987f
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2015-10-10 16:45:31 +0200

    add postalCodeLookup.php

---
+++ common.inc.php
@@ -0,0 +1,22 @@
+<?php
+
+function GET_or($name, $default) {
+    return isset($_GET[$name]) ? $_GET[$name] : $default;
+}
+function GET_or_NULL($name) {
+    return GET_or($name, NULL);
+}
+
+function echoJson($o) {
+
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+    }
+   
+  header('Content-type: application/json; charset=UTF-8');
+  if (isset($_GET["callback"]))
+    echo $_GET["callback"] . "(" . json_encode($o) . ");";
+  else
+    echo json_encode($o);  
+}
---
+++ postalCodeLookup.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once 'common.inc.php';
+
+$postalcode= GET_or_NULL('postalcode');
+$country = GET_or_NULL('country');
+$token = GET_or_NULL('token');
+$maxRows = min(max(GET_or_NULL("maxRows"), 10), 99);
+
+if ($country === 'FR') {
+    require_once 'frenchPostalCodes.inc.php';
+    $towns = $frenchPostalCodes[$postalcode];
+    if (!$towns) {
+        $towns = $allTowns;
+    }
+    if ($token) {
+        $token = strtoupper($token);
+        $r1 = array();
+        $r2 = array();
+        foreach ($towns as $town) {
+            $pos = strpos($town, $token);
+            if ($pos !== FALSE) {
+                if ($pos === 0)
+                    $r1[] = $town;
+                else
+                    $r2[] = $town;
+            }
+        }
+        $towns = array_merge($r1, $r2);
+    }
+    echoJson(array("towns" => $towns));
+}
